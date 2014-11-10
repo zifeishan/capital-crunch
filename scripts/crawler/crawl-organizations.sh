1b75e2a6006924bbb062407bda41412cc3c55cf2
Zifei Shan <zifei@stanford.edu>
2014-11-09 17:48:01 -0800

    Added person crawler

--- scripts/crawler/crawl-organizations.sh
+++ scripts/crawler/crawl-organizations.sh
@@ -2,10 +2,12 @@
 mkdir -p output/organizations
 
 orglist="data/organization-list.txt"
+
 for org in $(cat $orglist); do
+# for org in $(comm -1 < (sort $orglist) < (sort `ls output/organizations/`)); do
   echo "Getting $org..."
-  curl -v  -X GET "http://api.crunchbase.com/v/2/organization/$org?user_key=$USER_KEY" > output/organizations/$org.json
-  sleep 0.3  # 200 requests / minute
+  curl --retry 3 --max-time 10 -X GET "http://api.crunchbase.com/v/2/organization/$org?user_key=$USER_KEY" > output/organizations/$org
+  sleep 0.5  # 200 requests / minute
 done
 
 # curl -v  -X GET "http://api.crunchbase.com/v/2/organization/$1?user_key=$USER_KEY" > organizations/$1.json
